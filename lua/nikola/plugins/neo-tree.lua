36c9f8653a4f178eac45bef0665a9cb0ad7100d3
Nikola Savic <nikola.savic@bay42.io>
2023-11-01 16:02:34 +0100

    [5] - added copilot and neotree migration update

--- lua/nikola/plugins/neo-tree.lua
+++ lua/nikola/plugins/neo-tree.lua
@@ -5,5 +5,34 @@ return {
       "nvim-lua/plenary.nvim",
       "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
       "MunifTanjim/nui.nvim",
-    }
+    },
+    config = function()
+    require("neo-tree").setup({
+      close_if_last_window = true,
+      window = {
+        width = 30,
+      },
+      buffers = {
+        follow_current_file = {
+          enable = true,
+        },
+      },
+      filesystem = {
+        follow_current_file = {
+          enable = true,
+        },
+        filtered_items = {
+          hide_dotfiles = false,
+          hide_gitignored = false,
+          hide_by_name = {
+            "node_modules"
+          },
+          never_show = {
+            ".DS_Store",
+            "thumbs.db"
+          },
+        },
+      },
+    })
+  end
 }
