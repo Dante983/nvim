2402d0ed15d6d0e8c8782e2ad6dfa10c24eca3e4
Nikola Savic <nikola.savic@bay42.io>
2023-11-10 12:57:51 +0100

    [6] - random dashboard config and telescope

--- lua/nikola/plugins/dashboard-nvim.lua
+++ lua/nikola/plugins/dashboard-nvim.lua
@@ -1,24 +1,88 @@
 return {
-  'nvimdev/dashboard-nvim',
-  event = 'VimEnter',
-  config = function()
-    require('dashboard').setup {
-      config = {
-        center = {
-          {
-            icon = '',
-            icon_hl = 'group',
-            desc = 'description',
-            desc_hl = 'group',
-            key = 'shortcut key in dashboard buffer not keymap !!',
-            key_hl = 'group',
-            key_format = ' [%s]', -- `%s` will be substituted with value of `key`
-            action = '',
+  {
+    "nvimdev/dashboard-nvim",
+    event = "VimEnter",
+    config = function()
+      ---@class Shortcut
+      ---@field icon string
+      ---@field icon_hl string|nil
+      ---@field desc string
+      ---@field desc_hl string|nil
+      ---@field key string
+      ---@field key_hl string|nil
+      ---@field keymap string|nil
+      ---@field action string|fun()
+      ---@param shortcut Shortcut
+      ---@return Shortcut
+      local function key(shortcut)
+        shortcut.icon_hl = shortcut.icon_hl or "Title"
+        shortcut.desc_hl = shortcut.desc_hl or "String"
+        shortcut.key_hl = shortcut.key_hl or "Keyword"
+        return shortcut
+      end
+      require("dashboard").setup {
+        theme = "doom",
+        config = {
+          header = {
+            "",
+            "",
+            "",
+            "",
+            "",
+             "⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⣠⣤⣤⣴⣦⣤⣤⣄⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀ ",
+             "⠀⠀⠀⠀⠀⠀⢀⣤⣾⣿⣿⣿⣿⠿⠿⠿⠿⣿⣿⣿⣿⣶⣤⡀⠀⠀⠀⠀⠀⠀ ",
+             "⠀⠀⠀⠀⣠⣾⣿⣿⡿⠛⠉⠀⠀⠀⠀⠀⠀⠀⠀⠉⠛⢿⣿⣿⣶⡀⠀⠀⠀⠀ ",
+             "⠀⠀⠀⣴⣿⣿⠟⠁⠀⠀⠀⣶⣶⣶⣶⡆⠀⠀⠀⠀⠀⠀⠈⠻⣿⣿⣦⠀⠀⠀ ",
+             "⠀⠀⣼⣿⣿⠋⠀⠀⠀⠀⠀⠛⠛⢻⣿⣿⡀⠀⠀⠀⠀⠀⠀⠀⠙⣿⣿⣧⠀⠀ ",
+             "⠀⢸⣿⣿⠃⠀⠀⠀⠀⠀⠀⠀⠀⢀⣿⣿⣷⠀⠀⠀⠀⠀⠀⠀⠀⠸⣿⣿⡇⠀ ",
+             "⠀⣿⣿⡿⠀⠀⠀⠀⠀⠀⠀⠀⢀⣾⣿⣿⣿⣇⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⣿⠀ ",
+             "⠀⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⢠⣿⣿⡟⢹⣿⣿⡆⠀⠀⠀⠀⠀⠀⠀⣹⣿⣿⠀ ",
+             "⠀⣿⣿⣷⠀⠀⠀⠀⠀⠀⣰⣿⣿⠏⠀⠀⢻⣿⣿⡄⠀⠀⠀⠀⠀⠀⣿⣿⡿⠀ ",
+             "⠀⢸⣿⣿⡆⠀⠀⠀⠀⣴⣿⡿⠃⠀⠀⠀⠈⢿⣿⣷⣤⣤⡆⠀⠀⣰⣿⣿⠇⠀ ",
+             "⠀⠀⢻⣿⣿⣄⠀⠀⠾⠿⠿⠁⠀⠀⠀⠀⠀⠘⣿⣿⡿⠿⠛⠀⣰⣿⣿⡟⠀⠀ ",
+             "⠀⠀⠀⠻⣿⣿⣧⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣾⣿⣿⠏⠀⠀⠀ ",
+             "⠀⠀⠀⠀⠈⠻⣿⣿⣷⣤⣄⡀⠀⠀⠀⠀⠀⠀⢀⣠⣴⣾⣿⣿⠟⠁⠀⠀⠀⠀ ",
+             "⠀⠀⠀⠀⠀⠀⠈⠛⠿⣿⣿⣿⣿⣿⣶⣶⣿⣿⣿⣿⣿⠿⠋⠁⠀⠀⠀⠀⠀⠀ ",
+             "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠉⠛⠛⠛⠛⠛⠛⠉⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀ ",
+            "",
+            "",
+            "",
           },
+          center = {
+            {
+              icon = "  ",
+              desc = 'Find Files',
+              key = 'SPC f f',
+              -- keymap = 'SPC f f',
+              key_format = ' %s', -- remove default surrounding `[]`
+              action = 'lua print(3)'
+            },
+            {
+              icon = "  ",
+              desc = 'Find Recent Files',
+              key = 'SPC f r',
+              -- keymap = 'SPC f r',
+              key_format = ' %s', -- remove default surrounding `[]`
+              action = 'lua print(3)'
+            },
+            {
+              icon = "  ",
+              desc = 'Find Words in CWD',
+              key = 'SPC f s',
+              -- keymap = 'SPC f s',
+              key_format = ' %s', -- remove default surrounding `[]`
+              action = 'lua print(3)'
+            },
+            -- key { icon = "  ", desc = "FZF", key = "fw", action = "FzfLua" },
+            -- key { icon = "  ", desc = "Find Files", key = "ff", action = "FzfLua files" },
+            -- key { icon = "  ", desc = "Recent Files", key = "ff", action = "FzfLua files" },
+            -- key { icon = "  ", desc = "Find Word", key = "fr", action = "FzfLua oldfiles" },
+            -- key { icon = "󰒲  ", desc = "Plugins", key = "<SPC> l p", action = "Lazy" },
+            -- key { icon = "  ", desc = "Help", key = "fh", action = "FzfLua help_tags" },
+          },
+          footer = { "IDEMO NIS!!!" },
         },
-        footer = {},
       }
-    }
-  end,
-  dependencies = { {'nvim-tree/nvim-web-devicons'}}
+    end,
+  }
 }
