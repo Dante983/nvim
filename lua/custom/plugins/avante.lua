6241516b113da0aafc8370ee22bd6c89ef29c8e2
Nikola Savic <savicn209@gmail.com>
2025-02-25 10:45:17 +0100

    V4.0

--- lua/custom/plugins/avante.lua
+++ lua/custom/plugins/avante.lua
@@ -6,6 +6,13 @@ return {
   opts = {
     -- provider = 'openai',
     provider = 'claude',
+    claude = {
+      endpoint = 'https://api.anthropic.com',
+      -- model = 'claude-3-5-sonnet-20241022',
+      model = 'claude-3-7-sonnet-20250219',
+      temperature = 0,
+      max_tokens = 4096,
+    },
     -- auto_suggestions_provider = 'copilot',
     behavior = {
       auto_suggestions = false,
