a5e1b16192663abdf5d7c7dd7ba181577510d994
Nikola Savic <savicn209@gmail.com>
2025-10-16 00:18:43 +0200

    v3.2

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -4,10 +4,12 @@ return {
     dependencies = { "nvim-lua/plenary.nvim" },
     config = function()
         local builtin = require("telescope.builtin")
-        vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope find files" })
+        vim.keymap.set("n", "<leader>ff", function()
+            builtin.find_files({ hidden = true, follow = true, no_ignore = true })
+        end, { desc = "Find all files" })
         vim.keymap.set("n", "<leader>fw", builtin.live_grep, { desc = "Telescope live grep" })
         vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
         vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope help tags" })
-        vim.keymap.set("n", "<leader>fo", builtin.old_files, { desc = "Telescope old files" })
+        vim.keymap.set("n", "<leader>fo", builtin.oldfiles, { desc = "Telescope old files" })
     end,
 }
