813823ae69cda220e6d18e0d78408bee586e05f9
Nikola Savic <nikola@performcb.com>
2025-10-24 16:11:40 +0200

    v3.7

--- lua/plugins/database.lua
+++ lua/plugins/database.lua
@@ -1,26 +1,29 @@
 return {
-	{
-		"kristijanhusak/vim-dadbod-ui",
-		dependencies = {
-			{ "tpope/vim-dadbod", lazy = true },
-			{ "kristijanhusak/vim-dadbod-completion", ft = { "sql", "mysql", "plsql" }, lazy = true }, -- Optional
-		},
-		cmd = {
-			"DBUI",
-			"DBUIToggle",
-			"DBUIAddConnection",
-			"DBUIFindBuffer",
-		},
-		init = function()
-			-- Your DBUI configuration
-			vim.g.db_ui_use_nerd_fonts = 1
-			vim.g.dbs = {
-				{
-					-- cbleap local
-					name = "docker-mysql-cbleap",
-					url = "mysql://root:secret@127.0.0.1:3306/cbleap",
-				},
-			}
-		end,
-	},
+    {
+        "kristijanhusak/vim-dadbod-ui",
+        dependencies = {
+            { "tpope/vim-dadbod",                     lazy = true },
+            { "kristijanhusak/vim-dadbod-completion", ft = { "sql", "mysql", "plsql" }, lazy = true }, -- Optional
+        },
+        cmd = {
+            "DBUI",
+            "DBUIToggle",
+            "DBUIAddConnection",
+            "DBUIFindBuffer",
+        },
+        init = function()
+            -- Your DBUI configuration
+            vim.g.db_ui_use_nerd_fonts = 1
+            vim.g.dbs = {
+                {
+                    -- cbleap local
+                    name = "docker-mysql-cbleap",
+                    url = "mysql://root:secret@127.0.0.1:3306/cbleap",
+                },
+            }
+        end,
+        keymaps = {
+            vim.keymap.set("n", "<leader>B", "<cmd>DBUIToggle<CR>", { desc = "DB toggle" }),
+        },
+    },
 }
