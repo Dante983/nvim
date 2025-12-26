44d26ee66af11d1d1ac7eb6f97c4a00d05a0a864
Nikola Savic <nikola@performcb.com>
2025-12-26 12:32:05 +0100

    v4.6

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -4,6 +4,14 @@ return {
     lazy = false,
     ---@type snacks.Config
     opts = {
+        lazygit = {
+            config = {
+                os = {
+                    edit =
+                    '[ -z ""$NVIM"" ] && (nvim -- {{filename}}) || (nvim --server ""$NVIM"" --remote-send ""q"" && nvim --server ""$NVIM"" --remote {{filename}})',
+                },
+            },
+        },
         bigfile = { enabled = true },
         dashboard = { enabled = false },
         explorer = {
